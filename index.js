4f18c9e48c4247a1d92e4431dba469bf99bf83a3
Dantallope <dantebraccialini@gmail.com>
2022-12-14 13:08:23 -0700

    package.json stuffs' '

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message:'what is your name'
+        }
+    ])
